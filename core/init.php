34df7f617d5ecf692699cbc55012cf8a859ecd12
mohamed salah <Mohamed Salah@EngMohamedSalah.(none)>
2017-03-26 01:28:34 +0200

    change db pass

--- core/init.php
+++ core/init.php
@@ -4,18 +4,19 @@
   //paths
   define('BASE_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/msp/');
   //Include Configration
-  require_once(BASE_URL.'/config/config.php');
+  set_include_path('../');
+  require_once('config/config.php');
 //echo BASE_URL;
   // Helper Function Files
-  require_once(BASE_URL.'/helpers/system_helper.php');
-  require_once(BASE_URL.'/helpers/format_helper.php');
-  require_once(BASE_URL.'/helpers/db_helper.php');
+  require_once('helpers/system_helper.php');
+  require_once('helpers/format_helper.php');
+  require_once('helpers/db_helper.php');
 
 
 
 //echo BASE_URL;
   //Autoload Classes
  function __autoload($class_name){
-    require_once(BASE_URL."libraries/". $class_name . '.php');
+    require_once("libraries/". $class_name . '.php');
   }
 ?>
