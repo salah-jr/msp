de8f35f2a8c17d920da3737b50268df34de7a154
mohamed salah <mohamed salah>
2017-03-26 00:54:27 +0200

    change db pass

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 //DB params
   define('DB_HOST','localhost');
   define('DB_USER','root');
-  define('DB_PASS','');
+  define('DB_PASS','root');
   define('DB_NAME','msp');
 
   define('SITE_TITLE',"MSP Tech-Club");
